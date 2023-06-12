3d20f24cc7b949df8c11f487c1997731781d66b4
ozank <ozankoksal@hotmail.com>
2023-06-12 22:36:25 +0300

    First solution

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+fn main() -> std::io::Result<()> {
+    let file = File::open("input.txt").expect("file not found!");
+    let buf_reader = BufReader::new(file);
+
+    let mut v: Vec<i32> = Vec::new();
+    v.push(0);
+    let mut idx: usize = 0;
+
+    for line in buf_reader.lines() {
+        let line1 = line.unwrap();
+        if "".eq(&line1) {
+            idx += 1;
+            v.push(0);
+        } else {
+            v[idx] += line1.parse::<i32>().unwrap();
+        }
+    }
+    let max = v.iter().max().unwrap();
+
+    let index = v.iter().position(|element| element == max).unwrap();
+    println!(
+        "{} th elf is the one with the most calories with {} calories",
+        index + 1,
+        max
+    );
+    Ok(())
 }
